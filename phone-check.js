7e04a1e7ee810b2860707d66e9fc5e417e033b8a
rjenk18 <65114918+rjenk18@users.noreply.github.com>
2021-03-17 08:47:51 -0400

    Update phone-check.js

--- phone-check.js
+++ phone-check.js
@@ -1,46 +1,10 @@
-function telephoneCheck(str) {
+function telephoneCheck(str) {  
 
-  let hasTenDigits = false;
-  let hasElevenDigits = false;
-  let startWithOne = false;
-  let hasPermittedCharsOnly = false;
-  let hasCorrectParentheses = false;
-
-  if (str[0] === "1") {
-    str = str.slice(1, str.length);
-  } if (str[0] === " ") {
-    str = str.slice(1, str.length);
-  }
-
-  if (str[0] === "(" && str[4] === ")") {
-    hasCorrectParentheses = true;
-  }
-
-  if (str.match(/\d/g).length === 10) {
-    hasTenDigits = true;
-  };
-
- // if (str.match(/^\(\d{3}\)/) || str.match(/\d{3}/)) {
- //   hasCorrectParentheses = true;
- // }
-
-  if (str.match(/[\s\d\-]/)) {
-    hasPermittedCharsOnly = true;
-  }
-
-  if (!hasTenDigits) {
-    return false;
-  } else if (!hasPermittedCharsOnly) {
-    return false;
-  }  else if (!hasCorrectParentheses) {
-    return false;
-  } else {
+  if (/^1?\s?(\(\d{3}\)|\d{3})[- ]?(\d{3})[- ]?(\d{4})$/.test(str)) {
     return true;
+  } else {
+    return false;
   }
 }
 
 telephoneCheck("555-555-5555");
-
-
-
-  let numCheck = /^(1\s?)?[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
